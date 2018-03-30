943619815db7d2c465691726223c30d2495a5e82
Olwynrosy <lauriane.elena.fels@gmail.com>
2018-03-30 12:09:52 +0200

    1er dépot

---
+++ Script/script1.js
@@ -0,0 +1,230 @@
+document.getElementById("BL416").style.display = "none";
+document.getElementById("BL111").style.display = "none";
+document.getElementById("BL58").style.display = "none";
+document.getElementById("BL163").style.display = "none";
+document.getElementById("BL37").style.display = "none";
+document.getElementById("BL195").style.display = "none";
+document.getElementById("BL160").style.display = "none";
+document.getElementById("BL208").style.display = "none";
+document.getElementById("BL320").style.display = "none";
+document.getElementById("BL1600").style.display = "none";
+document.getElementById("BL360").style.display = "none";
+document.getElementById("BL264").style.display = "none";
+document.getElementById("BL1601").style.display = "none";
+document.getElementById("BL240").style.display = "none";
+document.getElementById("BL352").style.display = "none";
+document.getElementById("BL353").style.display = "none";
+document.getElementById("BL351").style.display = "none";
+document.getElementById("BL269").style.display = "none";
+document.getElementById("BL290").style.display = "none";
+
+
+document.getElementById("AL416").onclick = function() {
+  if (document.getElementById("AL416").value == "En savoir plus")
+  {
+    document.getElementById("BL416").style.display = "block";
+    document.getElementById("AL416").value = "Masquer";
+  }
+  else {
+    document.getElementById("BL416").style.display = "none";
+    document.getElementById("AL416").value = "En savoir plus";
+  }
+}
+document.getElementById("AL111").onclick = function() {
+  if (document.getElementById("AL111").value == "En savoir plus")
+  {
+    document.getElementById("BL111").style.display = "block";
+    document.getElementById("AL111").value = "Masquer";
+  }
+  else {
+    document.getElementById("BL111").style.display = "none";
+    document.getElementById("AL111").value = "En savoir plus";
+  }
+}
+document.getElementById("AL58").onclick = function() {
+  if (document.getElementById("AL58").value == "En savoir plus")
+  {
+    document.getElementById("BL58").style.display = "block";
+    document.getElementById("AL58").value = "Masquer";
+  }
+  else {
+    document.getElementById("BL58").style.display = "none";
+    document.getElementById("AL58").value = "En savoir plus";
+  }
+}
+document.getElementById("AL163").onclick = function() {
+  if (document.getElementById("AL163").value == "En savoir plus")
+  {
+    document.getElementById("BL163").style.display = "block";
+    document.getElementById("AL163").value = "Masquer";
+  }
+  else {
+    document.getElementById("BL163").style.display = "none";
+    document.getElementById("AL163").value = "En savoir plus";
+  }
+}
+document.getElementById("AL37").onclick = function() {
+  if (document.getElementById("AL37").value == "En savoir plus")
+  {
+    document.getElementById("BL37").style.display = "block";
+    document.getElementById("AL37").value = "Masquer";
+  }
+  else {
+    document.getElementById("BL37").style.display = "none";
+    document.getElementById("AL37").value = "En savoir plus";
+  }
+}
+document.getElementById("AL195").onclick = function() {
+  if (document.getElementById("AL195").value == "En savoir plus")
+  {
+    document.getElementById("BL195").style.display = "block";
+    document.getElementById("AL195").value = "Masquer";
+  }
+  else {
+    document.getElementById("BL195").style.display = "none";
+    document.getElementById("AL195").value = "En savoir plus";
+  }
+}
+document.getElementById("AL160").onclick = function() {
+  if (document.getElementById("AL160").value == "En savoir plus")
+  {
+    document.getElementById("BL160").style.display = "block";
+    document.getElementById("AL160").value = "Masquer";
+  }
+  else {
+    document.getElementById("BL160").style.display = "none";
+    document.getElementById("AL160").value = "En savoir plus";
+  }
+}
+document.getElementById("AL208").onclick = function() {
+  if (document.getElementById("AL208").value == "En savoir plus")
+  {
+    document.getElementById("BL208").style.display = "block";
+    document.getElementById("AL208").value = "Masquer";
+  }
+  else {
+    document.getElementById("BL208").style.display = "none";
+    document.getElementById("AL208").value = "En savoir plus";
+  }
+}
+document.getElementById("AL320").onclick = function() {
+  if (document.getElementById("AL320").value == "En savoir plus")
+  {
+    document.getElementById("BL320").style.display = "block";
+    document.getElementById("AL320").value = "Masquer";
+  }
+  else {
+    document.getElementById("BL320").style.display = "none";
+    document.getElementById("AL320").value = "En savoir plus";
+  }
+}
+document.getElementById("AL1600").onclick = function() {
+  if (document.getElementById("AL1600").value == "En savoir plus")
+  {
+    document.getElementById("BL1600").style.display = "block";
+    document.getElementById("AL1600").value = "Masquer";
+  }
+  else {
+    document.getElementById("BL1600").style.display = "none";
+    document.getElementById("AL1600").value = "En savoir plus";
+  }
+}
+document.getElementById("AL360").onclick = function() {
+  if (document.getElementById("AL360").value == "En savoir plus")
+  {
+    document.getElementById("BL360").style.display = "block";
+    document.getElementById("AL360").value = "Masquer";
+  }
+  else {
+    document.getElementById("BL360").style.display = "none";
+    document.getElementById("AL360").value = "En savoir plus";
+  }
+}
+document.getElementById("AL264").onclick = function() {
+  if (document.getElementById("AL264").value == "En savoir plus")
+  {
+    document.getElementById("BL264").style.display = "block";
+    document.getElementById("AL264").value = "Masquer";
+  }
+  else {
+    document.getElementById("BL264").style.display = "none";
+    document.getElementById("AL264").value = "En savoir plus";
+  }
+}
+document.getElementById("AL1601").onclick = function() {
+  if (document.getElementById("AL1601").value == "En savoir plus")
+  {
+    document.getElementById("BL1601").style.display = "block";
+    document.getElementById("AL1601").value = "Masquer";
+  }
+  else {
+    document.getElementById("BL1601").style.display = "none";
+    document.getElementById("AL1601").value = "En savoir plus";
+  }
+}
+document.getElementById("AL240").onclick = function() {
+  if (document.getElementById("AL240").value == "En savoir plus")
+  {
+    document.getElementById("BL240").style.display = "block";
+    document.getElementById("AL240").value = "Masquer";
+  }
+  else {
+    document.getElementById("BL240").style.display = "none";
+    document.getElementById("AL240").value = "En savoir plus";
+  }
+}
+document.getElementById("AL352").onclick = function() {
+  if (document.getElementById("AL352").value == "En savoir plus")
+  {
+    document.getElementById("BL352").style.display = "block";
+    document.getElementById("AL352").value = "Masquer";
+  }
+  else {
+    document.getElementById("BL352").style.display = "none";
+    document.getElementById("AL352").value = "En savoir plus";
+  }
+}
+document.getElementById("AL353").onclick = function() {
+  if (document.getElementById("AL353").value == "En savoir plus")
+  {
+    document.getElementById("BL353").style.display = "block";
+    document.getElementById("AL353").value = "Masquer";
+  }
+  else {
+    document.getElementById("BL353").style.display = "none";
+    document.getElementById("AL353").value = "En savoir plus";
+  }
+}
+document.getElementById("AL351").onclick = function() {
+  if (document.getElementById("AL351").value == "En savoir plus")
+  {
+    document.getElementById("BL351").style.display = "block";
+    document.getElementById("AL351").value = "Masquer";
+  }
+  else {
+    document.getElementById("BL351").style.display = "none";
+    document.getElementById("AL351").value = "En savoir plus";
+  }
+}
+document.getElementById("AL269").onclick = function() {
+  if (document.getElementById("AL269").value == "En savoir plus")
+  {
+    document.getElementById("BL269").style.display = "block";
+    document.getElementById("AL269").value = "Masquer";
+  }
+  else {
+    document.getElementById("BL269").style.display = "none";
+    document.getElementById("AL269").value = "En savoir plus";
+  }
+}
+document.getElementById("AL290").onclick = function() {
+  if (document.getElementById("AL290").value == "En savoir plus")
+  {
+    document.getElementById("BL290").style.display = "block";
+    document.getElementById("AL290").value = "Masquer";
+  }
+  else {
+    document.getElementById("BL290").style.display = "none";
+    document.getElementById("AL290").value = "En savoir plus";
+  }
+}
